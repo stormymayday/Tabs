2d172953ab41b766c51153eb32039e1eec0204e8
stormymayday <stormymayday7@gmail.com>
2023-03-09 17:56:29 -0700

    Selecting all articles with class of content.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const about = document.querySelector('.about');
 const tabBtns = document.querySelectorAll('.tab-btn');
-console.log(about);
+const contentArticles = document.querySelectorAll('.content');
+console.log(contentArticles);
