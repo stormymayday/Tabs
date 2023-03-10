29998ad9ff4a894ec09edd0ac4e4cae595d6fcdd
stormymayday <stormymayday7@gmail.com>
2023-03-09 20:19:02 -0700

    Moved adding class 'active' functionality outside the forEach.

--- app.js
+++ app.js
@@ -8,17 +8,15 @@ about.addEventListener('click', function (event) {
 
     if (id) {
 
+        // removing active class from all the buttons
         tabBtns.forEach(function (btn) {
 
-            // removing active class from all the buttons
             btn.classList.remove('active');
-
-            // adding class 'active' to the button that was clicked
-            event.target.classList.add('active');
 
         });
 
-
+        // adding class 'active' to the button that was clicked
+        event.target.classList.add('active');
 
         // 
 
